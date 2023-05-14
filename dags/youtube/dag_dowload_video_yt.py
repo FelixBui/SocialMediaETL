347f2e46f4adaf1c930526a77e677a0a49f51c57
shenkedokato <shenkedokato@gmail.com>
2023-05-15 01:47:52 +0700

    debug dag

--- dags/youtube/dag_dowload_video_yt.py
+++ dags/youtube/dag_dowload_video_yt.py
@@ -3,31 +3,10 @@ from pytube import YouTube
 import pytube
 from pathlib import Path
 from moviepy.editor import *
-from datetime import datetime, date, timedelta
+from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.models import Variable
 
-default_args = {
-    "owner": "shen",
-    "retries": 1,
-    "retry_delay": timedelta(minutes=1),
-    "depends_on_past": False,
-    "email": ['shenkedokato@gmail.com'] ,
-    "sla": timedelta(hours=1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-}
-dag = DAG(
-    dag_id="download_video_from_yt",
-    default_args=default_args,
-    description= 'dag download video from youtube',
-    schedule_interval = "@once",
-    start_date=datetime(2023, 5, 15),
-    catchup=False,
-    tags=["testing"]
-) 
 
 def download_video():
     video_id = '5gfS8MydNLk'
@@ -38,12 +17,18 @@ def download_video():
     os.rename(yt, name_file_mp4)
 
 
-with dag:
-    start_task = DummyOperator(task_id = "start")
-    end_task = DummyOperator(task_id = "end")
+with DAG(
+    dag_id="download_video_from_yt",
+    start_date=datetime(2023, 5, 15),
+    schedule = "@once",
+    catchup=False,
+) as dag:
+    task = PythonOperator(task_id = "dl_video", python_callable=download_video)
 
-    first_task = PythonOperator(task_id = "hello_word", python_callable=download_video)
-    start_task >> first_task >> end_task
+
+
+    
+
 
 
 
