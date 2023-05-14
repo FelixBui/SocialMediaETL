551d2e9fdb67b78d6dc35f77f4a561c5bea3180f
shenkedokato <shenkedokato@gmail.com>
2023-05-15 01:30:31 +0700

    add dag_dl_video

---
+++ dags/youtube/dag_dowload_video_yt.py
@@ -0,0 +1,54 @@
+import os
+from pytube import YouTube
+import pytube
+from pathlib import Path
+from moviepy.editor import *
+from datetime import datetime, date, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+
+default_args = {
+    "owner": "shen",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+    "depends_on_past": False,
+    "email": ['shenkedokato@gmail.com'] ,
+    "sla": timedelta(hours=1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+dag = DAG(
+    dag_id="download_video_from_yt",
+    default_args=default_args,
+    description= 'dag download video from youtube',
+    schedule_interval = "@once",
+    start_date=datetime(2023, 5, 15),
+    catchup=False,
+    tags=["testing"]
+) 
+
+def download_video():
+    video_id = '5gfS8MydNLk'
+    video_url = f'https://www.youtube.com/watch?v={video_id}'
+
+    # Download YouTube video in 480p resolution using pytube
+    try:
+        yt = YouTube(video_url).streams.get_by_itag(22).download()
+        name_file_mp4 = f'{video_id}.mp4'
+        os.rename(yt, name_file_mp4 )
+    except Exception E:
+        print(e)
+
+
+with dag:
+    start_task = DummyOperator(task_id = "start")
+    end_task = DummyOperator(task_id = "end")
+
+    first_task = PythonOperator(task_id = "hello_word", python_callable=download_video)
+    start_task >> first_task >> end_task
+# Set up YouTube video ID and URL
+
+
+
--- dags/youtube/dowload_video_yt.py
+++
@@ -1,6 +0,0 @@
-import pytube
-
-link = "https://www.youtube.com/watch?v=LsMcjV3LJ1U"
-yt=pytube.YouTube(link)
-yt.streams.get_lowest_resolution().download()
-print("dowload",link)
