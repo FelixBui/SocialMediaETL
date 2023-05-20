8a386b62df892e8acceba6d8d692844ddb62936c
shenkedokato <shenkedokato@gmail.com>
2023-05-20 15:43:53 +0700

    add dag dl

--- dags/youtube/dag_dowload_video_yt.py
+++ dags/youtube/dag_dowload_video_yt.py
@@ -9,15 +9,16 @@ from airflow.contrib.operators.ssh_operator import SSHOperator
 
 dag = DAG(
     dag_id="download_video_from_yt",
-    start_date=datetime(2023, 5, 17),
+    start_date=datetime(2023, 5, 20),
     schedule = "@once",
     catchup=False,
 )
 
-def download_youtube_video(video_id, video_path):
+def download_youtube_video(video_id):
+    video_id='5gfS8MydNLk'
     video_url = f'https://www.youtube.com/watch?v={video_id}'
     # Download YouTube video in 480p resolution using pytube
-    yt = YouTube(video_url).streams.get_by_itag(22).download(output_path=video_path,filename=f'{video_id}.mp4')
+    yt = YouTube(video_url).streams.get_by_itag(22).download(filename=f'{video_id}.mp4')
 
 
 with dag:
@@ -25,20 +26,8 @@ with dag:
     download_task = PythonOperator(
         task_id='download_video',
         python_callable=download_youtube_video,
-        op_kwargs={
-            'video_url': '5gfS8MydNLk',
-            'video_path': '/home/philongqh/'
-        }
     )
 
-    # Copy file từ Compute Engine đến local
-    copy_task = SSHOperator(
-        task_id='copy_video',
-        ssh_conn_id='compute_engine',
-        command='gcloud compute scp /home/philongqh/*.mp4 philongqh@LOCAL_MACHINE_IP:/path/to/local/folder',
-    )
-
-download_task >> copy_task
 
 
     
