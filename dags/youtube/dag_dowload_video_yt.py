879c7076f9f65dcd94ff39902ef3fc6527c8afef
shenkedokato <shenkedokato@gmail.com>
2023-05-28 12:47:51 +0700

    fdsaf

--- dags/youtube/dag_dowload_video_yt.py
+++ dags/youtube/dag_dowload_video_yt.py
@@ -1,17 +1,31 @@
 import os
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
 from pytube import YouTube
 import pytube
-from datetime import datetime
+from datetime import datetime, date, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
 
-
+default_args = {
+    "owner": "longtp",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+    "depends_on_past": False,
+    "email": ['philongqh@gmail.com'] ,
+    "sla": timedelta(hours=1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
 dag = DAG(
-    dag_id="download_video_from_yt",
-    start_date=datetime(2023, 5, 21),
-    schedule = "@once",
+    dag_id="youtube.hellword",
+    default_args=default_args,
+    description= 'My first dag',
+    schedule_interval = "@once",
+    start_date=datetime(2023, 1, 3),
     catchup=False,
-)
+    tags=["testing"]
+) 
 
 def download_youtube_video():
     video_id= '1XoICkGxWtw'
@@ -21,11 +35,14 @@ def download_youtube_video():
 
 
 with dag:
-    # tải video từ youtube và lưu trữ trên Compute Engine
-    download_task = PythonOperator(
-        task_id='download_video',
-        python_callable=download_youtube_video,
-    )
+    start_task = DummyOperator(task_id = "start")
+    end_task = DummyOperator(task_id = "end")
+
+    first_task = PythonOperator(task_id = "hello_word", python_callable=download_youtube_video)
+    start_task >> first_task >> end_task
+
+
+
 
 
 
