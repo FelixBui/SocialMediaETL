717f29f2ebb5662d9b8a2794bb1c902d4375db54
shenkedokato <shenkedokato@gmail.com>
2023-05-30 23:20:00 +0700

    add cd

--- scripts/dl_yt.py
+++ scripts/dl_yt.py
@@ -6,5 +6,5 @@ import os
 video_id= '1XoICkGxWtw'
 video_url = f'https://www.youtube.com/watch?v={video_id}'
 file_name= f'{video_id}.mp4'
-video_path=f'E:/Python'
+video_path=f'/opt/airflow/data/yt'
 yt = YouTube(video_url).streams.get_by_itag(22).download(output_path=video_path,filename=file_name)
