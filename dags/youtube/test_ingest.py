593268e9e2b366fa8eac2120e00194c9515cf880
longtp <philongqh@gmail.com>
2023-06-11 19:31:48 +0700

    test import

--- dags/youtube/test_ingest.py
+++ dags/youtube/test_ingest.py
@@ -1,21 +1,43 @@
 from datetime import datetime, date, timedelta
-import sys
-import os
-# Append the root directory of your project to sys.path
-root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(root_dir)
-# from data.raw.youtube.ingest_ytb import Ingest_YTB
-import plugins.helpers.utils import *
-
-
-# def testing():
-#     import os
-#     video_url = 'https://www.youtube.com/shorts/0Vf1TpucUss'
-#     src_file_path = "/otp/airflow/0Vf1TpucUss.mp4"
-#     ytb_loader = Ingest_YTB('youtube')
-#     ytb_loader.execute(video_url, src_file_path)
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 
 
 
-# if __name__ == '__main__':
-#     testing()
+def testing():
+    import os
+    video_url = 'https://www.youtube.com/shorts/0Vf1TpucUss'
+    src_file_path = "/otp/airflow/0Vf1TpucUss.mp4"
+    ytb_loader = Ingest_YTB('youtube')
+    ytb_loader.execute(video_url, src_file_path)
+
+def test_import():
+    from plugins.helpers.utils import *
+
+default_args = {
+    "owner": "longtp",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+    "depends_on_past": False,
+    "email": ['philongqh@gmail.com'] ,
+    "sla": timedelta(hours=1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+dag = DAG(
+    dag_id="ytb.test_import",
+    default_args=default_args,
+    schedule_interval = "@once",
+    start_date=datetime(2023, 1, 3),
+    catchup=False,
+    tags=["testing"]
+) 
+
+with dag:
+    start_task = DummyOperator(task_id = "start")
+    end_task = DummyOperator(task_id = "end")
+
+    test_import_task = PythonOperator(task_id = "hello_word", python_callable=test_import())
+    start_task >> test_import_task >> end_task
+
